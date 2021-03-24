f3dd7b05b3f8d4bee29ee1392023615fde3e3286
wbh <wang.bohao@outlook.com>
2021-03-24 11:05:54 +0800

    CSKD has been down

---
+++ dataset/dataloader_CSKD.py
@@ -0,0 +1,275 @@
+import warnings
+from collections import defaultdict
+import bisect
+
+import numpy as np
+import os
+import random
+import torchvision
+from torch.utils.data import Sampler, Dataset, DataLoader, BatchSampler, SequentialSampler, RandomSampler
+from torchvision import transforms, datasets
+
+
+# 继承了Sampler类
+class PairBatchSampler(Sampler):
+    # 本代码当中num_iterations用到的地方都为None
+    def __init__(self, dataset, batch_size, num_iterations=None):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.num_iterations = num_iterations
+
+    # iter用来产生迭代索引值的
+    def __iter__(self):
+        # 获取索引值0～N-1
+        indices = list(range(len(self.dataset)))
+        random.shuffle(indices)
+
+        # 一次完整的循环代表一个epoch；len代表 数据总量/batchsize
+        for k in range(len(self)):
+            if self.num_iterations is None:
+                offset = k * self.batch_size
+                batch_indices = indices[offset:offset + self.batch_size]
+            else:
+                batch_indices = random.sample(range(len(self.dataset)), self.batch_size)
+
+            # 随机选择上面随机选择的batch数据当中同类别的数据索引
+            pair_indices = []
+            for idx in batch_indices:
+                y = self.dataset.get_class(idx)  # idx这个数据获取所在的类的编号？
+                # random.choice随机返回列表中某一个项 dataset是datawrapper的一个对象
+                pair_indices.append(random.choice(self.dataset.classwise_indices[y]))
+
+            # 返回一个list 前一半代表与后一半是同类别关系
+            yield batch_indices + pair_indices
+
+    # 用来返回每次迭代器的长度
+    def __len__(self):
+        if self.num_iterations is None:
+            return (len(self.dataset) + self.batch_size - 1) // self.batch_size
+        else:
+            return self.num_iterations
+
+
+# 继承自dataset
+class DatasetWrapper(Dataset):
+    # Additional attributes
+    # - indices
+    # - classwise_indices
+    # - num_classes
+    # - get_class
+
+    def __init__(self, dataset, indices=None):
+        self.base_dataset = dataset
+        if indices is None:
+            self.indices = list(range(len(dataset)))  # 使用默认的indices
+        else:
+            self.indices = indices
+
+        # torchvision 0.2.0 compatibility
+        if torchvision.__version__.startswith('0.2'):
+            if isinstance(self.base_dataset, datasets.ImageFolder):
+                self.base_dataset.targets = [s[1] for s in self.base_dataset.imgs]
+            else:
+                if self.base_dataset.train:
+                    self.base_dataset.targets = self.base_dataset.train_labels
+                else:
+                    self.base_dataset.targets = self.base_dataset.test_labels
+
+        # defaultdict当key在字典当中不存在时，返回的是默认值 对于list是[]
+        self.classwise_indices = defaultdict(list)  # classwise_indices作用是根据类别查找该类别的数据样本
+        for i in range(len(self)):
+            y = self.base_dataset.targets[self.indices[i]]
+            self.classwise_indices[y].append(i)  # y这个方法当中添加i这个下标的数据
+        self.num_classes = max(self.classwise_indices.keys()) + 1  # 类别数量
+
+    #  loads and returns a sample from the dataset at the given index idx
+    def __getitem__(self, i):
+        return self.base_dataset[self.indices[i]]
+
+    # 返回dataset当中的sample的个数
+    def __len__(self):
+        return len(self.indices)
+
+    # 根据数据的下标返回这个数据所属的类别
+    def get_class(self, i):
+        return self.base_dataset.targets[self.indices[i]]
+
+
+# 貌似没用到？
+class ConcatWrapper(Dataset):  # TODO: Naming
+    @staticmethod  # 静态方法调用无需实例化
+    def cumsum(sequence):
+        r, s = [], 0
+        for e in sequence:
+            l = len(e)
+            r.append(l + s)
+            s += l
+        return r
+
+    @staticmethod
+    def numcls(sequence):
+        s = 0
+        for e in sequence:
+            l = e.num_classes
+            s += l
+        return s
+
+    @staticmethod
+    def clsidx(sequence):
+        r, s, n = defaultdict(list), 0, 0
+        for e in sequence:
+            l = e.classwise_indices
+            for c in range(s, s + e.num_classes):
+                t = np.asarray(l[c - s]) + n
+                r[c] = t.tolist()
+            s += e.num_classes
+            n += len(e)
+        return r
+
+    def __init__(self, datasets):
+        super(ConcatWrapper, self).__init__()
+        assert len(datasets) > 0, 'datasets should not be an empty iterable'
+        self.datasets = list(datasets)
+        # for d in self.datasets:
+        #     assert not isinstance(d, IterableDataset), "ConcatDataset does not support IterableDataset"
+        self.cumulative_sizes = self.cumsum(self.datasets)
+
+        self.num_classes = self.numcls(self.datasets)
+        self.classwise_indices = self.clsidx(self.datasets)
+
+    def __len__(self):
+        return self.cumulative_sizes[-1]
+
+    def __getitem__(self, idx):
+        if idx < 0:
+            if -idx > len(self):
+                raise ValueError("absolute value of index should not exceed dataset length")
+            idx = len(self) + idx
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+        return self.datasets[dataset_idx][sample_idx]
+
+    def get_class(self, idx):
+        if idx < 0:
+            if -idx > len(self):
+                raise ValueError("absolute value of index should not exceed dataset length")
+            idx = len(self) + idx
+        dataset_idx = bisect.bisect_right(self.cumulative_sizes, idx)
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
+        true_class = self.datasets[dataset_idx].base_dataset.targets[self.datasets[dataset_idx].indices[sample_idx]]
+        return self.datasets[dataset_idx].base_dataset.target_transform(true_class)
+
+    @property  # 可以将方法当作属性一样使用
+    def cummulative_sizes(self):
+        warnings.warn("cummulative_sizes attribute is renamed to "
+                      "cumulative_sizes", DeprecationWarning, stacklevel=2)
+        return self.cumulative_sizes
+
+
+def load_dataset(name, root, sample='default', **kwargs):
+    if name in ['imagenet', 'tinyimagenet', 'CUB200', 'STANFORD120', 'MIT67']:
+        if name == 'tinyimagenet':
+            transform_train = transforms.Compose([
+                transforms.RandomResizedCrop(32),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+            transform_test = transforms.Compose([
+                transforms.Resize(32),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+
+            train_val_dataset_dir = os.path.join(root, "train")
+            test_dataset_dir = os.path.join(root, "val")
+
+            # 通用图像加载器 图像按照文件夹进行分类
+            trainset = DatasetWrapper(datasets.ImageFolder(root=train_val_dataset_dir, transform=transform_train))
+            valset = DatasetWrapper(datasets.ImageFolder(root=test_dataset_dir, transform=transform_test))
+
+        elif name == 'imagenet':
+            transform_train = transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+            transform_test = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+            train_val_dataset_dir = os.path.join(root, "train")
+            test_dataset_dir = os.path.join(root, "val")
+
+            trainset = DatasetWrapper(datasets.ImageFolder(root=train_val_dataset_dir, transform=transform_train))
+            valset = DatasetWrapper(datasets.ImageFolder(root=test_dataset_dir, transform=transform_test))
+
+        else:
+            transform_train = transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+            transform_test = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+
+            train_val_dataset_dir = os.path.join(root, name, "train")
+            test_dataset_dir = os.path.join(root, name, "test")
+
+            trainset = DatasetWrapper(datasets.ImageFolder(root=train_val_dataset_dir, transform=transform_train))
+            valset = DatasetWrapper(datasets.ImageFolder(root=test_dataset_dir, transform=transform_test))
+
+    elif name.startswith('CIFAR'):
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        if name == 'CIFAR10':
+            CIFAR = datasets.CIFAR10
+        else:
+            CIFAR = datasets.CIFAR100
+
+        trainset = DatasetWrapper(CIFAR(root, train=True, download=True, transform=transform_train))
+        valset = DatasetWrapper(CIFAR(root, train=False, download=True, transform=transform_test))
+    else:
+        raise Exception('Unknown dataset: {}'.format(name))
+
+    # Sampler
+    if sample == 'default':
+        get_train_sampler = lambda d: BatchSampler(RandomSampler(d), kwargs['batch_size'], False)
+        get_test_sampler = lambda d: BatchSampler(SequentialSampler(d), kwargs['batch_size'], False)
+
+    elif sample == 'pair':
+        get_train_sampler = lambda d: PairBatchSampler(d, kwargs['batch_size'])  # 自定义的sampler
+        get_test_sampler = lambda d: BatchSampler(SequentialSampler(d), kwargs['batch_size'], False)
+
+    else:
+        raise Exception('Unknown sampling: {}'.format(sample))
+
+    # Dataloader的sampler参数若被指定则shuffle参数失效
+    trainloader = DataLoader(trainset, batch_sampler=get_train_sampler(trainset), num_workers=4)
+    valloader = DataLoader(valset, batch_sampler=get_test_sampler(valset), num_workers=4)
+
+    return trainloader, valloader
---
+++ train_CSKD.py
@@ -0,0 +1,222 @@
+import argparse
+import logging
+import os
+import time
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+import models
+import utils
+from dataset.dataloader_CSKD import load_dataset
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', default='0', type=str)
+parser.add_argument("--lr", type=float, default=0.1)
+parser.add_argument("--num_epochs", type=int, default=300)
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--dataset", type=str, default="CIFAR100")
+parser.add_argument("--outdir", type=str, default="save_CSKD_model")
+parser.add_argument("--model", type=str, default="resnet32")
+parser.add_argument("--wd", type=float, default=1e-4)
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--dropout', default=0., type=float, help='Input the dropout rate: default(0.0)')
+parser.add_argument('--cls', '-cls', action='store_true', default=True, help='adding cls loss')
+parser.add_argument('--temp', default=4.0, type=float, help='temperature scaling')
+parser.add_argument('--lamda', default=1.0, type=float, help='cls loss weight ratio')
+args = parser.parse_args()
+
+torch.backends.cudnn.benchmark = True  # 对于固定不变的网络可以起到优化作用
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = "cpu"
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """decrease the learning rate at 100 and 150 epoch"""
+    lr = args.lr
+    if epoch >= 0.5 * args.num_epochs:
+        lr /= 10
+    if epoch >= 0.75 * args.num_epochs:
+        lr /= 10
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+# 与model的用法相同，重载了forward函数
+class KDLoss(nn.Module):
+    def __init__(self, temp_factor):
+        super().__init__()
+        self.temp_factor = temp_factor
+        self.kl_div = nn.KLDivLoss(reduction="sum")
+
+    def forward(self, input, target):
+        log_p = torch.log_softmax(input / self.temp_factor, dim=1)  # 学生
+        q = torch.softmax(target / self.temp_factor, dim=1)  # 教师
+        # TODO：KLDivLoss到底怎么用？target的位置？
+        loss = self.kl_div(log_p, q) * (self.temp_factor ** 2) / input.size(0)
+        return loss
+
+
+def train(train_loader, model, optimizer, criterion1, criterion_kd):
+    model.train()
+    loss_avg = utils.AverageMeter()
+    accTop1_avg = utils.AverageMeter()
+    accTop5_avg = utils.AverageMeter()
+    loss_kd = utils.AverageMeter()
+    end = time.time()
+
+    with tqdm(total=len(train_loader)) as t:
+        for i, (train_batch, labels_batch) in enumerate(train_loader):
+            train_batch = train_batch.cuda(non_blocking=True)
+            labels_batch = labels_batch.cuda(non_blocking=True)
+            batch_size = train_batch.size(0)
+            # 取minibatch当中的前一半sample作为与label求loss的样本
+            targets_ = labels_batch[:batch_size // 2]
+            outputs = model(train_batch[:batch_size // 2])
+
+            loss = criterion1(outputs, targets_)
+
+            with torch.no_grad():
+                outputs_cls = model(train_batch[batch_size // 2:])
+            cls_loss = criterion_kd(outputs, outputs_cls.detach())
+            loss += args.lamda * cls_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loss_kd.update(cls_loss.item(), batch_size // 2)
+            loss_avg.update(loss.item(), batch_size // 2)
+
+            metrics = utils.accuracy(outputs, targets_, topk=(1, 5))  # metircs代表指标
+            accTop1_avg.update(metrics[0].item())
+            accTop5_avg.update(metrics[1].item())
+
+            t.update()
+
+    train_metrics = {'train_loss': loss_avg.value(),
+                     'loss_kd': loss_kd.value(),
+                     'train_accTop1': accTop1_avg.value(),
+                     'train_accTop5': accTop5_avg.value(),
+                     'time': time.time() - end}
+
+    metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in train_metrics.items())
+    logging.info("- Train metrics: " + metrics_string)
+    return train_metrics
+
+
+def evaluate(test_loader, model, criterion):
+    model.eval()
+    loss_avg = utils.AverageMeter()
+    accTop1_avg = utils.AverageMeter()
+    accTop5_avg = utils.AverageMeter()
+    end = time.time()
+
+    with torch.no_grad():
+        for test_batch, labels_batch in test_loader:
+            test_batch = test_batch.cuda(non_blocking=True)
+            labels_batch = labels_batch.cuda(non_blocking=True)
+
+            output_batch = model(test_batch)
+            loss = criterion(output_batch, labels_batch)
+
+            metrics = utils.accuracy(output_batch, labels_batch, topk=(1, 5))
+            # only one element tensors can be converted to Python scalars
+            accTop1_avg.update(metrics[0].item())
+            accTop5_avg.update(metrics[1].item())
+            loss_avg.update(loss.item(), test_batch.size(0))
+
+    # compute mean of all metrics in summary
+    test_metrics = {'test_loss': loss_avg.value(),
+                    'test_accTop1': accTop1_avg.value(),
+                    'test_accTop5': accTop5_avg.value(),
+                    'time': time.time() - end}
+
+    metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in test_metrics.items())
+    logging.info("- Test  metrics: " + metrics_string)
+    return test_metrics
+
+
+if __name__ == '__main__':
+    begin_time = time.time()
+    if not os.path.exists(args.outdir):
+        print("Directory does not exist! Making directory {}".format(args.outdir))
+        os.makedirs(args.outdir)
+        os.makedirs(args.outdir + "/resume")
+        os.makedirs(args.outdir + "/log")
+
+    now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    utils.set_logger(os.path.join(args.outdir, 'log', now_time + 'train.log'))
+    w = vars(args)
+    metrics_string = " ;\n".join("{}: {}".format(k, v) for k, v in w.items())
+    logging.info("- All args are followed: \n" + metrics_string)
+
+    logging.info("Loading the datasets...")
+    if args.dataset == 'CIFAR10':
+        num_classes = 10
+        model_folder = "model_cifar"
+        root = './Data'
+    elif args.dataset == 'CIFAR100':
+        num_classes = 100
+        model_folder = "model_cifar"
+        root = './Data'
+    elif args.dataset == 'imagenet':
+        num_classes = 1000
+        model_folder = "model_imagenet"
+        root = './Data'
+
+    if not args.cls:
+        train_loader, test_loader = load_dataset(args.dataset, root, batch_size=args.batch_size)
+    else:
+        train_loader, test_loader = load_dataset(args.dataset, root, 'pair', batch_size=args.batch_size)
+    logging.info("- Done.")
+
+    model_fd = getattr(models, model_folder)
+    if "resnet" in args.model:
+        model_cfg = getattr(model_fd, 'resnet')
+        model = getattr(model_cfg, args.model)(num_classes=num_classes)
+    elif "vgg" in args.model:
+        model_cfg = getattr(model_fd, 'vgg')
+        model = getattr(model_cfg, args.model)(num_classes=num_classes, dropout=args.dropout)
+    elif "densenet" in args.model:
+        model_cfg = getattr(model_fd, 'densenet')
+        model = getattr(model_cfg, args.model)(num_classes=num_classes)
+
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model).to(device)
+    else:
+        model = model.to(device)
+
+    num_params = (sum(p.numel() for p in model.parameters()) / 1000000.0)
+    logging.info('Total params: %.2fM' % num_params)
+
+    criterion1 = nn.CrossEntropyLoss()
+    criterion2 = KDLoss(args.temp)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, nesterov=True, weight_decay=args.wd)
+
+    best_acc = 0
+    for i in range(args.num_epochs):
+        logging.info("Epoch {}/{}".format(i + 1, args.num_epochs))
+        train_metrics = train(train_loader, model, optimizer, criterion1, criterion2)
+        test_metrics = evaluate(test_loader, model, criterion1)
+        test_acc = test_metrics['test_accTop1']
+        if test_acc >= best_acc:
+            logging.info("- Found better accuracy")
+            best_acc = test_acc
+
+            last_path = os.path.join(args.outdir, 'resume', 'best_model.pth')
+            save_dic = {'state_dict': model.state_dict(),
+                        'optim_dict': optimizer.state_dict(),
+                        'epoch': i + 1,
+                        'test_accTop1': test_metrics['test_accTop1'],
+                        'test_accTop5': test_metrics['test_accTop5']}
+            torch.save(save_dic, last_path)
+
+        adjust_learning_rate(optimizer, i)
+    logging.info('Total time: {:.2f} minutes'.format((time.time() - begin_time) / 60.0))
+    logging.info('All tasks have been done!')
